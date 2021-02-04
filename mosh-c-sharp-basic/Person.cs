bb58309ee477f8fa9dfba271d3fb0b885b2c971f
RMKendall <ryan.morris.kendall@gmail.com>
2021-02-03 23:59:33 -0600

    Complete Section 4 Non-primitive types

---
+++ mosh-c-sharp-basic/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mosh_c_sharp_basic
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int Age;
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hello, I am " + firstName + " " + lastName + "\n");
+        }
+
+       
+    }
+
+}
--- mosh-c-sharp-basic/Program.cs
+++ mosh-c-sharp-basic/Program.cs
@@ -2,6 +2,14 @@
 
 namespace mosh_c_sharp_basic
 {
+    //Enum: collection of constants; ints unless cast otherwise ex. : byte
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisterAirMail = 2,
+        Express = 3,
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -14,7 +22,7 @@ namespace mosh_c_sharp_basic
             var totalPrice = 20.95f;
             //Non-primitive types
             var character = 'a';
-            var fName = "that";
+            var fName = "That";
             var fact = true;
 
             Console.WriteLine(number);
@@ -71,7 +79,92 @@ namespace mosh_c_sharp_basic
             Console.WriteLine(n > m && n == p);
             Console.WriteLine(n != p || p > m);
 
+            //Single comment
+
+            /*
+             Multi-line comment
+            */
+
+            //Instance of person class
+            var ryan = new Person();
+            ryan.firstName = "Ryan";
+            ryan.lastName = "Kendall";
+            ryan.Age = 20;
+            ryan.Introduce();
+
+            MakeOld(ryan);
+            Console.WriteLine(ryan.Age + "\n");
+
+            //Arrays
+            var flags = new bool[3] { true, false, true };
+            var names = new string[3] { "Larry", "Moe", "Curly" };
+
+
+            /*
+            Important! When setting a variable that is non-primitive,
+            it is a reference type and is a class (vs structure).
+            The allocated memory address will hold the actual values.
+            The value stored for the variable will be an allocated memory address.
+            So if you set a non-primitive (reference type) variable to equal another,
+            they will hold the same value; the allocated memory address.
+            Therefore if you change the the contents within that address, it
+            changes it for both variables!
+            */
+            var numbers = new int[3] { 1, 2, 3 };
+            var moreNumbers = numbers;
+            
+            /*
+             This will change the value for index position 0 to zero in 
+             both moreNumbers and numbers!
+             */ 
+            moreNumbers[0] = 0;
+
+            
+
+            //Strings
+            var marineBase = "Camp Pendleton";
+            var lName = "Guy";
+
+            var aName = string.Format("{0} {1}", fName, lName);
+            var sameName = fName + " " + lName;
+
+            //Joining elements of an array
+            var theFellas = string.Join(", ", names);
+
+            Console.WriteLine(theFellas);
+
+            //Concat and newline escape characters
+            var message = "At " + marineBase + " is " + aName + ".\nHe is stationed with: \n" + theFellas + ".";
+            //Same result as above using different format
+  
+            //Verbatim string
+            var anotherMessage = @"Check this out.
+No escape character needed.
+To go to a new line.";
+
+            Console.WriteLine(message + "\n");
+            Console.WriteLine(string.Format("At {0} is {1}.\nHe is stationed with:\n{2}", marineBase, aName, theFellas));
+            Console.WriteLine("\n");
+            Console.WriteLine(anotherMessage);
+
+            //Enums
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+            Console.WriteLine(method.ToString());
+
+            var setMethod = 2;
+            Console.WriteLine((ShippingMethod)setMethod);
+
+            var anotherMethod = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), anotherMethod);
+
 
         }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
     }
+
 }
