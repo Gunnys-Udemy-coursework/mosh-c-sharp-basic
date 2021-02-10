ea2464232c45fcf71e8816726ca495fc2c8a732d
RMKendall <ryan.morris.kendall@gmail.com>
2021-02-09 20:09:09 -0600

    Array exercises complete

--- mosh-c-sharp-basic/Program.cs
+++ mosh-c-sharp-basic/Program.cs
@@ -352,7 +352,8 @@ namespace mosh_c_sharp_basic
             //ArrayExercise1();
             //ArrayExercise2();
             //ArrayExercise3();
-            ArrayExercise4();
+            //ArrayExercise4();
+            ArrayExercise5();
         }
 
        
@@ -484,7 +485,56 @@ namespace mosh_c_sharp_basic
             }
         }
 
-        public static void MakeOld(Person person)
+        public static void ArrayExercise5()
+        {
+            string[] elements;
+
+            while (true)
+            {
+                Console.Write("Enter a list of comma-separated numbers: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    elements = input.Split(',');
+                    if (elements.Length >= 5)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Invalid List");
+            }
+
+            var numbers = new List<int>();
+
+            foreach (var number in elements)
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+
+            var smallest = new List<int>();
+            while (smallest.Count < 3)
+            {
+                // Assume the first number is the smallest
+                var min = numbers[0];
+                foreach (var number in numbers)
+                {
+                    if (number < min)
+                        min = number;
+                }
+                smallest.Add(min);
+
+                numbers.Remove(min);
+            }
+
+            Console.WriteLine("The 3 smallest numbers are: ");
+            foreach (var number in smallest)
+                Console.WriteLine(number);
+        }
+    
+
+    public static void MakeOld(Person person)
         {
             person.Age += 10;
         }
