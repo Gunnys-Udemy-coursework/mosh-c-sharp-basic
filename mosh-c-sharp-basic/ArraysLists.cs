a4b6cd700f0a5e2ded01cafa38fb2a88e003bac4
RMKendall <ryan.morris.kendall@gmail.com>
2021-02-10 21:39:53 -0600

    Complete Section 8

--- mosh-c-sharp-basic/ArraysLists.cs
+++ mosh-c-sharp-basic/ArraysLists.cs
@@ -1,10 +1,184 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace mosh_c_sharp_basic
 {
     public class ArraysLists
     {
-        public ArraysLists()
+        public static void ArrayExercise1()
         {
+            var friends = new List<string>();
+            var reprompt = true;
+            do
+            {
+                Console.WriteLine("Enter a name or leave blank to end: ");
+                var friendName = Console.ReadLine();
+                if (string.IsNullOrEmpty(friendName))
+                {
+                    reprompt = false;
+                    break;
+                }
+                else
+                {
+                    friends.Add(friendName);
+                }
+            } while (reprompt);
+
+            var message = "";
+            switch (friends.Count)
+            {
+                case 0:
+                    break;
+                case 1:
+                    message = string.Format("{0} likes your post.", friends[0]);
+                    break;
+                case 2:
+                    message = string.Format("{0} and {1} like your post.", friends[0], friends[1]);
+                    break;
+                default:
+                    message = string.Format("{0}, {1} and {2} others like your post.", friends[0], friends[1], friends.Count - 2);
+                    break;
+            }
+
+            Console.WriteLine(message);
+        }
+
+        public static void ArrayExercise2()
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var nameArray = new char[name.Length];
+            var j = 0;
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                nameArray[j] = name[i];
+                j++;
+            }
+
+            var reversed = new string(nameArray);
+            Console.WriteLine(reversed);
+        }
+
+        public static void ArrayExercise3()
+        {
+            var numbers = new int[5];
+            var count = 0;
+            int choice;
+
+            while (count < 5)
+            {
+                Console.WriteLine("Enter a number: ");
+                var input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Invalid input");
+                }
+                else
+                {
+
+                    choice = Convert.ToInt32(input);
+
+                    if (Array.IndexOf(numbers, choice) == -1)
+                    {
+                        numbers[count] = choice;
+                        count++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
+                }
+            }
+
+            Array.Sort(numbers);
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+
+        public static void ArrayExercise4()
+        {
+            var numbers = new List<int>();
+            var uniqueNums = new List<int>();
+            var carryOn = true;
+
+            while (carryOn)
+            {
+                Console.WriteLine("Enter a number or Quit: ");
+                var input = Console.ReadLine();
+                if (input.Equals("Quit"))
+                {
+                    carryOn = false; ;
+                }
+                else
+                {
+                    numbers.Add(Convert.ToInt32(input));
+                }
+            }
+
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (uniqueNums.IndexOf(numbers[i]) == -1)
+                {
+                    uniqueNums.Add(numbers[i]);
+                }
+            }
+
+            for (var j = 0; j < uniqueNums.Count; j++)
+            {
+                Console.Write(uniqueNums[j] + " ");
+            }
+        }
+
+        public static void ArrayExercise5()
+        {
+            string[] elements;
+
+            while (true)
+            {
+                Console.Write("Enter a list of comma-separated numbers: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    elements = input.Split(',');
+                    if (elements.Length >= 5)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Invalid List");
+            }
+
+            var numbers = new List<int>();
+
+            foreach (var number in elements)
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+
+            var smallest = new List<int>();
+            while (smallest.Count < 3)
+            {
+                // Assume the first number is the smallest
+                var min = numbers[0];
+                foreach (var number in numbers)
+                {
+                    if (number < min)
+                        min = number;
+                }
+                smallest.Add(min);
+
+                numbers.Remove(min);
+            }
+
+            Console.WriteLine("The 3 smallest numbers are: ");
+            foreach (var number in smallest)
+                Console.WriteLine(number);
         }
     }
 }
--- mosh-c-sharp-basic/Program.cs
+++ mosh-c-sharp-basic/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace mosh_c_sharp_basic
 {
@@ -349,11 +350,11 @@ namespace mosh_c_sharp_basic
             ////Clear; removes everything from the list
             //nums2.Clear();
 
-            //ArrayExercise1();
-            //ArrayExercise2();
-            //ArrayExercise3();
-            //ArrayExercise4();
-            //ArrayExercise5();
+            //ArraysLists.ArrayExercise1();
+            //ArraysLists.ArrayExercise2();
+            //ArraysLists.ArrayExercise3();
+            //ArraysLists.ArrayExercise4();
+            //ArraysLists.ArrayExercise5();
 
             //Dates and Times
             //Creating
@@ -403,186 +404,45 @@ namespace mosh_c_sharp_basic
             var trimmedName = fullName.Replace(" ", "");
             Console.WriteLine(trimmedName);
 
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C2"));
+
+            //Summarising text
+            var sentence = "Yadda, yadda, yadda and it goes on and on and on.";
+            var summary = StringsText.SummarizeText(sentence);
+            Console.WriteLine(summary);
+
+            //String Builder
+            var builder = new StringBuilder();
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+            Console.WriteLine(builder);
+
+            builder.Replace('-', '+');
+            builder.Remove(0, 10); //index: 0, for 10 chars
+            builder.Insert(0, new string('*', 10));
+            Console.WriteLine(builder);
+
+            Console.WriteLine(builder[0]);
+
+            var cleanerBuild = new StringBuilder();
+            cleanerBuild
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10);
+            Console.WriteLine(cleanerBuild);
 
 
         }
 
        
 
-        public static void ArrayExercise1()
-        {
-            var friends = new List<string>();
-            var reprompt = true;
-            do
-            {
-                Console.WriteLine("Enter a name or leave blank to end: ");
-                var friendName = Console.ReadLine();
-                if (string.IsNullOrEmpty(friendName))
-                {
-                    reprompt = false;
-                    break;
-                }
-                else {
-                    friends.Add(friendName);
-                        }
-            } while (reprompt);
-
-            var message = "";
-            switch (friends.Count)
-            {
-                case 0:
-                    break;
-                case 1:
-                    message = string.Format("{0} likes your post.", friends[0]);
-                    break;
-                case 2:
-                    message = string.Format("{0} and {1} like your post.", friends[0], friends[1]);
-                    break;
-                default:
-                    message = string.Format("{0}, {1} and {2} others like your post.", friends[0], friends[1], friends.Count - 2);
-                    break;
-            }
-
-            Console.WriteLine(message);
-        }
-
-        public static void ArrayExercise2()
-        {
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            var nameArray = new char[name.Length];
-            var j = 0;
-
-            for(int i = name.Length -1; i >= 0; i--)
-            {
-                nameArray[j] = name[i];
-                j++;
-            }
-
-            var reversed = new string(nameArray);
-            Console.WriteLine(reversed);
-        }
-
-        public static void ArrayExercise3()
-        {
-            var numbers = new int[5];
-            var count = 0;
-            int choice;
-
-            while (count < 5)
-            {
-                Console.WriteLine("Enter a number: ");
-                var input = Console.ReadLine();
-                if (String.IsNullOrEmpty(input))
-                {
-                    Console.WriteLine("Invalid input");
-                }
-                else
-                {
-
-                    choice = Convert.ToInt32(input);
-
-                    if (Array.IndexOf(numbers, choice) == -1)
-                    {
-                        numbers[count] = choice;
-                        count++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input");
-                    }
-                }
-            }
-
-            Array.Sort(numbers);
-
-            for (var i = 0; i < numbers.Length; i++)
-            {
-                Console.Write(numbers[i] + " ");
-            }
-        }
-
-        public static void ArrayExercise4()
-        {
-            var numbers = new List<int>();
-            var uniqueNums = new List<int>();
-            var carryOn = true;
-
-            while (carryOn)
-            {
-                Console.WriteLine("Enter a number or Quit: ");
-                var input = Console.ReadLine();
-                if (input.Equals("Quit"))
-                {
-                    carryOn = false; ;
-                }
-                else
-                {
-                    numbers.Add(Convert.ToInt32(input));
-                }
-            }
-
-            for (var i = 0; i < numbers.Count; i++)
-            {
-                if (uniqueNums.IndexOf(numbers[i]) == -1)
-                {
-                    uniqueNums.Add(numbers[i]);
-                }
-            }
-
-            for (var j = 0; j < uniqueNums.Count; j++)
-            {
-                Console.Write(uniqueNums[j] + " ");
-            }
-        }
-
-        public static void ArrayExercise5()
-        {
-            string[] elements;
-
-            while (true)
-            {
-                Console.Write("Enter a list of comma-separated numbers: ");
-                var input = Console.ReadLine();
-
-                if (!String.IsNullOrWhiteSpace(input))
-                {
-                    elements = input.Split(',');
-                    if (elements.Length >= 5)
-                    {
-                        break;
-                    }
-                }
-
-                Console.WriteLine("Invalid List");
-            }
-
-            var numbers = new List<int>();
-
-            foreach (var number in elements)
-            {
-                numbers.Add(Convert.ToInt32(number));
-            }
-
-            var smallest = new List<int>();
-            while (smallest.Count < 3)
-            {
-                // Assume the first number is the smallest
-                var min = numbers[0];
-                foreach (var number in numbers)
-                {
-                    if (number < min)
-                        min = number;
-                }
-                smallest.Add(min);
-
-                numbers.Remove(min);
-            }
-
-            Console.WriteLine("The 3 smallest numbers are: ");
-            foreach (var number in smallest)
-                Console.WriteLine(number);
-        }
+        
     
 
     public static void MakeOld(Person person)
